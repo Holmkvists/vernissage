5544aa25d914bea12647b22de2a3c86b2b0f1533
Holmkvists <adamholmkvists@gmail.com>
2022-12-21 12:42:34 +0100

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 import "./scss/main.scss";
 
 function App() {
   return (
     <>
       <Header />
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Exhibition</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </nav>
+      <p>inquiries@holmkvist.com</p>
+    </footer>
+  );
+};
